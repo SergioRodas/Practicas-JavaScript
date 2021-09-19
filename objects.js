7e9f38b24da1a09a6f05827025070aaa775637b8
sergiorodas <sergiorodasbj@hotmail.com>
2021-09-19 20:36:16 -0300

    add objects

---
+++ objects.js
@@ -0,0 +1,15 @@
+const mascota = {
+    nombre : 'Tom',
+    edad : '10',
+    vivo : true,
+    razas : ['peludo', 'negro']
+}
+
+console.log(mascota.razas[1])
+console.log(mascota.nombre)
+console.log(mascota.edad)
+console.log(mascota.vivo)
+
+mascota.id = 1
+
+console.log(mascota.id)
